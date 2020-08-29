868010367c090e76c5033444532ffd4ed186bf1a
Zeyad Nofal <37900425+zeyadnofal@users.noreply.github.com>
2020-08-29 21:33:14 +0200

    29/8 - 9:33pm

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Document_Archiving
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((usernameBox.Text == "Admin") && (passwordBox.Password == "Admin"))
+            {
+                MessageBox.Show("Logged in successfully!\n Welcome Admin");
+            }
+            else
+            {
+                MessageBox.Show("Wrong login credentials.");
+            }
+        }
+    }
+}
